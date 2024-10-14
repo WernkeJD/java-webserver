bbb9d18e8b346ff951f1d90be228add64b54ac40
WernkeJD <Jacob.Wernke@yahoo.com>
2024-10-13 21:53:16 -0400

    final upload

--- Webserver/WebServer.java
+++ Webserver/WebServer.java
@@ -1,7 +1,10 @@
+package webserver;
+
 /**
  * Assignment 1
  * Jacob Wernke
- **/
+**/
+
 import java.io.*;
 import java.net.*;
 import java.util.*;
@@ -50,6 +53,34 @@ final class HttpRequest implements Runnable {
         }
     }
 
+    private static void sendBytes(FileInputStream fis, OutputStream os) throws Exception {
+        // Construct a 1K buffer to hold bytes on their way to the socket.
+        byte[] buffer = new byte[1024];
+        int bytes = 0;
+    
+        // Copy requested file into the socket's output stream.
+        while ((bytes = fis.read(buffer)) != -1) {
+            os.write(buffer, 0, bytes);
+        }
+    }
+    
+    private static String contentType(String fileName) {
+        if (fileName.endsWith(".htm") || fileName.endsWith(".html")) {
+            return "text/html";
+        }
+        if (fileName.endsWith(".jpeg")) {
+            System.out.println(fileName);
+            return "image/jpeg";
+        }
+        if (fileName.endsWith(".gif")) {
+            return "image/gif";
+        }
+        if(fileName.endsWith(".txt")){
+            return "text/plain";
+        }
+        return "application/octet-stream";
+    }
+
     private void processRequest() throws Exception {
         // Get a reference to the socket's input and output streams.
         InputStream is = socket.getInputStream();
@@ -92,25 +123,49 @@ final class HttpRequest implements Runnable {
             System.out.println(statusLine);
             System.out.println(contentTypeLine);
         } else {
-            statusLine = "HTTP/1.1 404 Not Found" + CRLF;;
-            contentTypeLine = "Content-type: text/html" + CRLF;
-            entityBody = "<HTML>" + 
-                         "<HEAD><TITLE>Not Found</TITLE></HEAD>" +
-                         "<BODY>Not Found</BODY></HTML>";
-            System.out.println("Response:");
-            System.out.println("-------------");
-            System.out.println(statusLine);
-            System.out.println(contentTypeLine);
+            if(!contentType(fileName).equalsIgnoreCase("text/plain")){
+                statusLine = "HTTP/1.1 404 Not Found" + CRLF;;
+                contentTypeLine = "Content-type: text/html" + CRLF;
+                entityBody = "<HTML>" + 
+                             "<HEAD><TITLE>Not Found</TITLE></HEAD>" +
+                             "<BODY>Not Found</BODY></HTML>";
+                System.out.println("Response:");
+                System.out.println("-------------");
+                System.out.println(statusLine);
+                System.out.println(contentTypeLine);
+            }else{
+                statusLine = "HTTP/1.1 200 OK" + CRLF;
+                contentTypeLine = "text/html; charset=UTF-8" + CRLF;
+
+                FtpClient ftp = new FtpClient();
+
+                ftp.connect("comp_net_user", "comp_net");
+
+                ftp.getFile(fileName);
+
+                ftp.disconnect();
+
+                fis = new FileInputStream(fileName);
+                fileExists = true;
+            }
         }
     
+        System.out.println("Response:");
+        System.out.println("------------");
+
         // Send the status line.
         os.writeBytes(statusLine);
+        System.out.println(statusLine);
     
         // Send the content type line.
         os.writeBytes(contentTypeLine);
+        System.out.println(contentTypeLine);
+
     
         // Send a blank line to indicate the end of the header lines.
         os.writeBytes(CRLF);
+        System.out.println(CRLF);
+
     
         // Send the entity body.
         if (fileExists) {
@@ -123,32 +178,7 @@ final class HttpRequest implements Runnable {
         // Close streams and socket.
         os.close();
         fis.close();
-    }
-    private static void sendBytes(FileInputStream fis, OutputStream os) throws Exception {
-        // Construct a 1K buffer to hold bytes on their way to the socket.
-        byte[] buffer = new byte[1024];
-        int bytes = 0;
-    
-        // Copy requested file into the socket's output stream.
-        while ((bytes = fis.read(buffer)) != -1) {
-            os.write(buffer, 0, bytes);
-        }
-    }
-    
-    private static String contentType(String fileName) {
-        if (fileName.endsWith(".htm") || fileName.endsWith(".html")) {
-            return "text/html";
-        }
-        if (fileName.endsWith(".jpeg")) {
-            return "image/jpeg";
-        }
-        if (fileName.endsWith(".gif")) {
-            return "image/gif";
-        }
-        if(fileName.endsWith(".txt")){
-            return "text/plain";
-        }
-        return "application/octet-stream";
+        socket.close();
     }
 }
 
